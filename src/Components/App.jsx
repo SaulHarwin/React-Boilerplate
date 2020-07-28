bf36ec9d97e7a9b55fdb3e767d3da7e2855286fb
SaulHarwin <15swharwin@uckfield.college>
2020-07-28 12:34:28 +0100

    Initial Commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>This is the App Component</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/style/css/style.css';
+
+// Component Imports
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
